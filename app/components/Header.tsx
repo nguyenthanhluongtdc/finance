5d7a849c2befaa47db2c741b7c024dac2cda86a0
Nguyenthanhluongtdc <ntluong@ambition.vn>
2023-10-07 15:10:08 +0700

    no msg

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
               component="a"
               href="#app-bar-with-responsive-menu"
               sx={{
-                height: {xs:'40px', md:'70px'},
+                height: {xs:'48px', sm:'64px'},
                 mr: 2,
                 minHeight:'auto',
                 fontFamily: 'monospace',
@@ -27,7 +27,7 @@ export default function Header() {
                 textDecoration: 'none',
               }}
             >
-              <Image alt='Logo' sizes="100vw" width={0} height={0} style={{width:'auto', height:'100%', display:'block'}} src={'./images/logo.jpg'} />
+              <Image alt='Logo' sizes="100vw" width={0} height={0} style={{width:'auto', height:'100%', display:'block'}} src={process.env.pathImage+'logo.jpg'} />
 
             </Typography>
               <Typography variant="h5" color="inherit" component="div">
--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /**
  * @type {import('next').NextConfig}
  */
+
+const env = process.env.NODE_ENV
+let pathImage = "";
+if(env == "development"){
+   pathImage = "/images/";
+}
+else if (env == "production"){
+    pathImage = "./images/";
+}
+
 const nextConfig = {
-    output: 'export',
+    // output: 'export',
+    env: {
+        pathImage: pathImage
+    }
 }
  module.exports = nextConfig
