7aa2002389444fffdd6f7a7cd6f562f9e94fd92a
Nguyenthanhluongtdc <ntluong@ambition.vn>
2023-10-07 19:07:31 +0700

    no msg

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,11 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter, Roboto } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import Header from './components/Header';
 import Container from '@mui/material/Container'
 import Footer from './components/Footer'
 
-const roboto = Roboto({
-  subsets: ['latin'],
-  weight: ['400', '700']
-})
-const inter = Inter({ subsets: ['cyrillic'] })
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: "vaytieudung247",
@@ -26,7 +22,7 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <body className={roboto.className}>
+      <body className={inter.className}>
           <div className='mb-3 sm:mb-4 md:mb-9'>
             <Header />
           </div>
